40371fc91b5b46b231259c15f2d5ae6c3bfa1a34
Toby Lee <toby@epril.com>
2012-01-17 23:16:43 +1000

    fixed

--- src/enable/ConfigurationClass.java
+++ src/enable/ConfigurationClass.java
@@ -1,11 +1,19 @@
 package enable;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportAware;
+import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 import enable.bean.Hello;
@@ -23,17 +31,31 @@ public class ConfigurationClass {
  	}
 	
 	@Configuration
-	@EnableHello(name="Spring")
+	@EnableHello(type=1, name="Toby")
 	public static class AppConfig {
 	}
 
-	@Import(HelloConfig.class)
-	@interface EnableHello {
+	@Retention(RetentionPolicy.RUNTIME)
+	@Import(HelloConfigSelector.class)
+	public @interface EnableHello {
+		int type();
 		String name();
 	}
 	
+	static class HelloConfigSelector implements ImportSelector {
+		@Override
+		public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+			int type = (Integer) importingClassMetadata.getAnnotationAttributes(
+					EnableHello.class.getName()).get("type");
+			
+			return type == 1 ? new String[] { HelloConfig1.class.getName() } :
+				new String[] { HelloConfig2.class.getName() };
+		}
+		
+	}
+	
 	@Configuration
-	public static class HelloConfig implements ImportAware {
+	public static class HelloConfig1 implements ImportAware {
 		@Bean Hello hello() {
 			Hello hello = new Hello();
 			return hello;
@@ -47,6 +69,22 @@ public class ConfigurationClass {
 					importMetadata.getAnnotationAttributes(
 							EnableHello.class.getName()).get("name"));
 		}
+	}
+	
+	@Configuration
+	public static class HelloConfig2 implements ImportAware {
+		@Bean Hello hello() {
+			Hello hello = new Hello();
+			return hello;
+		}
 		
+		@Autowired Hello hello;
+		
+		@Override
+		public void setImportMetadata(AnnotationMetadata importMetadata) {
+			hello.setName((String) 
+					importMetadata.getAnnotationAttributes(
+							EnableHello.class.getName()).get("name")+"2");
+		}
 	}
 }
