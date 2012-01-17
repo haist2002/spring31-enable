b5bbabe3685b27aaa0ddbc9c8500dc40f4386404
Toby Lee <toby@epril.com>
2012-01-17 19:10:22 +1000

    config

--- src/enable/ConfigurationClass.java
+++ src/enable/ConfigurationClass.java
@@ -1,5 +1,8 @@
 package enable;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import enable.bean.Hello;
@@ -7,10 +10,27 @@ import enable.support.BeanDefinitionUtils;
 
 public class ConfigurationClass {
 	public static void main(String[] args) {
-		GenericXmlApplicationContext ac = new GenericXmlApplicationContext(ConfigurationClass.class, "context.xml");
+//		GenericXmlApplicationContext ac = new GenericXmlApplicationContext(ConfigurationClass.class, "context.xml");
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+		
 		BeanDefinitionUtils.printBeanDefinitions(ac);
 		
 		Hello hello = ac.getBean(Hello.class);
 		hello.sayHello();
+		
+		AppConfig appConfig = ac.getBean(AppConfig.class);
+		System.out.println(hello);
+		System.out.println(appConfig.hello());
+		System.out.println(appConfig.hello());
+		System.out.println(appConfig.hello());
  	}
+	
+	@Configuration
+	public static class AppConfig {
+		@Bean Hello hello() {
+			Hello hello = new Hello();
+			hello.setName("Toby");
+			return hello;
+		}
+	}
 }
